d49068a1adbd72fc45d96acabe93e22239770bbd
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-03-25 17:27:41 +0300

    12

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import {Container, Title} from './styled'
+import React, {useState, useRef} from 'react'
+import {Container, Title, InfoMessage, SubContainer} from './styled'
 
 
 const Content = () => {
+    const [messageBoxProperties, setMessageBoxProperties] = useState({
+        opacity: 0,
+        textValue: ''
+    })
+
+    const messageInput = useRef()
+
+    function summonMessage(){
+        setMessageBoxProperties( (prevState) => {return {...prevState, textValue: messageInput.current.value, opacity: 100} } )
+    }
+
     return(
         <Container>
-            <Title>Смотри. Добавляй. Поделись</Title>
+            <SubContainer>
+                <Title>Title Text</Title>
+                {messageBoxProperties.textValue && <InfoMessage> {messageBoxProperties.textValue} </InfoMessage>}
+                <input ref={messageInput} placeholder='Message' type='text' style={{maxHeight: '100px'}}/>
+                <button onClick={summonMessage}>asd</button>
+            </SubContainer>
         </Container>
     )
 }
--- src/components/Content/styled.js
+++ src/components/Content/styled.js
@@ -2,11 +2,32 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
     &{
+        display: flex;
         flex: 1 1 auto;
         align-self: center;
         min-height: 100%;
         max-width: 80%;
         padding-top: 10px;
+    }
+`
+
+export const SubContainer = styled.div`
+    &{
+        display: flex;
+        flex-direction: column;
+        padding: 10px 0px;
+        height: fit-content
+    }
+`
+
+export const InfoMessage = styled.div`
+    &{
+        opacity: ${props => {return props.opacity ? props.opacity : '0'}};
+        transition: 1s;
+        border: 1px solid red;
+        padding: 5px;
+        align-self: center;
+        max-width: 360px;
     }
 `
 
